4363029e3db7b919017ea9e1a756f770caa00c68
rafailfilis <rafilos93@gmail.com>
2024-11-22 13:36:49 +0200

    m

--- markov_modelling.R
+++ markov_modelling.R
@@ -1,6 +1,6 @@
 
 library(dplyr)
-
+library(ggplot2)
 # markov chains (basic case)
 
 n_cohort <- 1000  # num of patients at first
@@ -70,22 +70,121 @@ total_costs <- numeric(nrow(markov_data))
 
 total_qalys <- numeric(nrow(markov_data))
  
-for(i in 2:nrow(markov_data)){
+
+ calculate_payoff <- function(data){
+for(i in 2:nrow(data)){
      
-      payoff_trace[i,] <- as.matrix(markov_data[i, -1]) %*% payoff
+      payoff_trace[i,] <- as.matrix(data[i, -1]) %*% payoff
       
       # Calculate the total cost for this step
         total_costs[i] <- sum(payoff_trace[, 1])
         total_qalys[i] <- sum(payoff_trace[,2])
-       
+      
 }
 
-payoff_trace<- cbind(payoff_trace, total_costs, total_qalys)
+     payoff_trace <- cbind(payoff_trace, total_costs, total_qalys)
+   }
+
+payoff_trace <- calculate_payoff(markov_data)
 
 #  total costs and qalys 
  c_total <- sum(payoff_trace[,1])/n_cohort
  q_total <- sum(payoff_trace[,2])/n_cohort
 
 
-
+# plot the markov model simulation progress
+p <- ggplot(data= markov_data , aes(x = cycle)) +
+    geom_point(aes(y = healthy), colour = "blue") +
+   geom_point(aes(y = diseased), colour = "green") + 
+   geom_point(aes(y = dead), color = "red") +
+   theme_light() +
+  labs(
+      x = "Cycle",
+      y = "Different states",
+      title = "Markov simulation progress" 
+  )  +
+  theme(
+      title = element_text(size = 30, margin = margin(b = 10)),
+      axis.title = element_text(size = 20)
+  )
+   
+print(p)
 # time-varying probabilities (what if transition_probs change when time passes by)
+
+# e.x. from 1 to 10 cycle where qx = 0.05
+#            healthy  diseased         dead 
+#   healthy    #     0.10               qx
+#   diseased   0        #           0.15 + qx
+#   dead       0           0             1
+
+
+transition_matrix1 <- array(NA, dim = c(n_states, n_states, n_cycle)) 
+
+qx <- 0.04
+ 
+transition_matrix1[3,3,] <- 1
+
+transition_matrix1[1, 2, 1:10] <- 0.15
+transition_matrix1[1, 3, 1:10] <- 0.04 #qx
+transition_matrix1[2, 3, 1:10] <- 0.15 + transition_matrix1[1, 3, 1:10]
+transition_matrix1[2, 2, 1:10] <- 1 - transition_matrix1[2, 3, 1:10]
+transition_matrix1[1,1, 1:10] <- 1 - transition_matrix1[1, 2, 1:10] - transition_matrix1[1, 3, 1:10]
+
+transition_matrix1[1, 2, 11:20] <- 0.15
+transition_matrix1[1, 3, 11:20] <- 0.12 #qx
+transition_matrix1[2, 3, 11:20] <- 0.15 + transition_matrix1[1, 3, 11:20]
+transition_matrix1[2, 2, 11:20] <- 1 - transition_matrix1[2, 3, 11:20]
+transition_matrix1[1,1, 11:20] <- 1 - transition_matrix1[1, 2, 11:20] - transition_matrix1[1, 3, 11:20]
+
+
+transition_matrix1[1, 2, 21:30] <- 0.15
+transition_matrix1[1, 3, 21:30] <- 0.13 #qx
+transition_matrix1[2, 3, 21:30] <- 0.15 + transition_matrix1[1, 3, 21:30]
+transition_matrix1[2, 2, 21:30] <- 1 - transition_matrix1[2, 3, 21:30]
+transition_matrix1[1,1, 21:30] <- 1 - transition_matrix1[1, 2, 21:30] - transition_matrix1[1, 3, 21:30]
+
+transition_matrix1[1, 2, 31:40] <- 0.15
+transition_matrix1[1, 3, 31:40] <- 0.15 #qx
+transition_matrix1[2, 3, 31:40] <- 0.15 + transition_matrix1[1, 3, 31:40]
+transition_matrix1[2, 2, 31:40] <- 1 - transition_matrix1[2, 3, 31:40]
+transition_matrix1[1,1, 31:40] <- 1 - transition_matrix1[1, 2, 31:40] - transition_matrix1[1, 3, 31:40]
+
+transition_matrix1[is.na(transition_matrix1)] <- 0
+
+
+markov_data1 <-  data.frame(
+    cycle = seq(1:n_cycle),
+    healthy = NA,
+    diseased = NA,
+    dead = NA
+)
+markov_data1[1,-1] <- c(n_cohort, 0, 0 )
+for(i in 2:nrow(markov_data1)){
+      markov_data1[i, -1] <- as.matrix(markov_data1[i-1, -1]) %*% transition_matrix1[, , i-1]
+}
+ 
+# calculate the costs and qualys 
+payoff_trace <- calculate_payoff(markov_data1)
+
+#  total costs and qalys - time-varying
+ c_total <- sum(payoff_trace[,1])/n_cohort
+ q_total <- sum(payoff_trace[,2])/n_cohort
+
+
+ # plot the markov model simulation progress (time-varying)
+p1 <- ggplot(data= markov_data1 , aes(x = cycle)) +
+    geom_point(aes(y = healthy), colour = "blue") +
+   geom_point(aes(y = diseased), colour = "green") + 
+   geom_point(aes(y = dead), color = "red") +
+   theme_light() +
+  labs(
+      x = "Cycle",
+      y = "Different states",
+      title = "Markov simulation progress (time-varying)" 
+  )  +
+  theme(
+      title = element_text(size = 30, margin = margin(b = 10)),
+      axis.title = element_text(size = 20)
+  )
+ 
+print(c_total)
